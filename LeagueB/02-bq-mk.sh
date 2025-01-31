7619564232bc7f9a4063c553d1d2384c6c85e363
cloud-jake <jake.holmquist+github@gmail.com>
2025-01-31 22:29:23 +0000

    2025

--- LeagueB/02-bq-mk.sh
+++ LeagueB/02-bq-mk.sh
@@ -5,12 +5,12 @@
 
 PROJECT_ID=bq-jake
 
-bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race1.def SectionOneSki.Race1
-bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race2.def SectionOneSki.Race2
-bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race3.def SectionOneSki.Race3
-bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race4.def SectionOneSki.Race4
-bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race5.def SectionOneSki.Race5
-bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race6.def SectionOneSki.Race6
+bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race1.def SectionOneSki2025.Race1
+bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race2.def SectionOneSki2025.Race2
+bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race3.def SectionOneSki2025.Race3
+bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race4.def SectionOneSki2025.Race4
+bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race5.def SectionOneSki2025.Race5
+bq --project_id=${PROJECT_ID} mk --table --external_table_definition=Race6.def SectionOneSki2025.Race6
 
 
 
